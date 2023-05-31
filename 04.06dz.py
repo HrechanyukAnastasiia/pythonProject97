b416b6f6ab56e07def2f8f75d0e0e353f3158d16
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-31 18:58:22 +0300

    3 завдання

---
+++ 04.06dz.py
@@ -0,0 +1,12 @@
+#3
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://uk.wikipedia.org/wiki/")
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, "html.parser")
+    classs = soup.find_all("exampel_class")
+    for i in classs:
+        print(i)
+else:
+    print(f"Немає підклчення {response.status_code}")
